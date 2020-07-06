916cb18fecb395aa930c60e0e89d574abfc4b79e
Mikhail Moshnikov <62326146+Mikleprog@users.noreply.github.com>
2020-07-06 23:25:09 +0300

    hw2

---
+++ src/app/testarray.ts
@@ -0,0 +1,20 @@
+export interface Card {
+  id: number
+  name: string
+  damage: number
+}
+
+export  const cards: Card[] =[
+ {id:158, name: 'Totodile', damage: 51},
+ {id: 159, name: 'Croconaw', damage: 51},
+ {id: 160, name: 'Feraligatr', damage: 70},
+ {id: 179, name: 'Mareep', damage: 25},
+ {id: 180, name: 'Flaaffy', damage: 20},
+ {id: 181, name: 'Ampharos', damage: 40},
+ {id: 182, name: 'Bellossom', damage: 35},
+ {id: 183, name: 'Marill', damage: 10},
+ {id: 184, name: 'Azumarill', damage: 20},
+ {id: 185, name: 'Sudowoodo', damage: 56},
+ {id: 186, name: 'Politoed', damage: 30},
+ {id: 187, name: 'Hoppip', damage: 22}
+];
