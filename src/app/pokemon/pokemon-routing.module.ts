ca29014705f04334445e485a7b7d2847f3aca45c
Mikhail Moshnikov <mikhail_moshnikov@epam.com>
2020-08-03 13:08:29 +0300

    HW5_edit_guard_messages

--- src/app/pokemon/pokemon-routing.module.ts
+++ src/app/pokemon/pokemon-routing.module.ts
@@ -5,12 +5,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { PokemonListComponent} from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 import { PokemonComponent } from './pokemon.component';
-import { AppComponent } from '../app.component'
+import { PokemonEditComponent } from './pokemon-edit/pokemon-edit.component';
+import { PokemonPopupComponent} from './pokemon-popup/pokemon-popup.component';
+import { ExitEditGuard } from './exit.edit.guard';
+
+import { AppComponent } from '../app.component';
 
 const pokemonRoutes: Routes = [
   { path: '', redirectTo: '/pokemon', pathMatch: 'full' },
   { path: 'pokemon', component: PokemonComponent },
-  { path: 'detail/:id', component: PokemonDetailComponent }
+  { path: 'pokemon/:id', component: PokemonDetailComponent },
+  { path: 'pokemon/:id/edit', component: PokemonEditComponent, canDeactivate: [ExitEditGuard]},
+  { path: 'messages', component: PokemonPopupComponent, outlet: 'messages'}
   
 ];
 
